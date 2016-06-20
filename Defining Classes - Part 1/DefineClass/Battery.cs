732e036ad1d856178c03d79d82fb472b420763cb
Anna Yanakieva <a.ianakieva@gmail.com>
2016-06-20 12:10:53 +0300

    Added Defining Classes - Part 1.sln

---
+++ Defining Classes - Part 1/DefineClass/Battery.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefineClass
+{   
+    public class Battery
+    {
+        private string batteryModel;
+        private double hoursIdle;
+        private double hoursTalk;
+        private BatteryType batteryType;
+
+        //constructors
+        public Battery(string batteryModel)
+        {
+            this.batteryModel = batteryModel;
+        }
+
+        public Battery(string batteryModel, BatteryType batteryType)
+            : this(batteryModel)
+        {
+            this.batteryType = batteryType;
+
+        }
+
+        //properties
+
+        public BatteryType BatteryType
+        {
+            get { return batteryType; }
+            set { batteryType = value; }
+        }  
+
+        public string BatteryModel
+        {
+            get { return this.batteryModel; }
+            set { this.batteryModel = value; }
+
+        }
+
+        public double HoursIdle
+        {
+            get { return this.hoursIdle; }
+            set { this.hoursIdle = value; }
+        }
+
+        public double HoursTalk
+        {
+            get { return this.hoursTalk; }
+            set { this.hoursTalk = value; }
+        }
+
+    }
+}
---
+++ Defining Classes - Part 1/DefineClass/GSMCallHistoryTest .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefineClass
+{
+   public class GSMCallHistoryTest
+    {
+        GSM newGSM = new GSM("iPhone", "6S", 1500, "me");
+       
+       
+        
+    }
+}
