55436f2ea547acebf869e294e86eba51bc8d0752
AnnaYanakieva <a.ianakieva@gmail.com>
2016-06-20 12:45:46 +0300

    Homework Defining Classes - Part 2 added

---
+++ Defining Classes - Part 2/Defining Classes - Part 2/MainMethod.cs
@@ -0,0 +1,33 @@
+﻿namespace DefiningClassesPart2
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Class1
+    {
+        static void Main()
+        {
+            Path test = new Path();
+           
+            Point3D point1 = new Point3D(1, 2.5, 3);
+            Point3D point2 = new Point3D(4, 5, 6);
+            Point3D point3 = new Point3D(7, 8, 9);
+
+            test.AddPoint(point1);      
+            test.AddPoint(point2);
+            test.AddPoint(point3);
+
+            Point3D point = new Point3D() { X = 1, Y = 1, Z = 1 };
+            Console.WriteLine(point);
+            Console.WriteLine(Point3D.StartOfCoordinateSystem);
+
+            var result = CalculateDistance.CalculateDistanceBetween2Points(point, Point3D.StartOfCoordinateSystem);
+            Console.WriteLine();
+            Console.WriteLine("Distance between point and start of coordinate system:\n{0}",result);
+            Console.WriteLine();
+            PathStorage.SavePath(test);
+            Console.WriteLine("Loaded paths:");
+            Console.WriteLine(PathStorage.ReadPath());           
+        }
+    }
+}
---
+++ Defining Classes - Part 2/GenericClass5-7/GenericListMain.cs
@@ -0,0 +1,29 @@
+﻿namespace GenericClass5_7
+{
+    using System;
+    using System.Collections.Generic;
+    class GenericListMain
+    {
+        static void Main(string[] args)
+        {
+            var testList = new GenericList<int>(8);
+            testList.Add(5);
+            testList.Add(15);
+            testList.Add(25);
+
+            // Console.WriteLine(testList[6]);
+            testList.RemoveAtIndex(1);
+            testList.InsertAtIndex(1, 322);
+            testList.InsertAtIndex(2, 422);
+            testList.InsertAtIndex(1, 522);
+
+            Console.WriteLine("Elements in List:");
+            Console.WriteLine(testList);
+            Console.WriteLine()
+                ;
+            //  Console.WriteLine(testList[3]); 
+            Console.WriteLine("Max element in list {0}", testList.Max());
+            Console.WriteLine("Min element in list {0}", testList.Min());
+        }
+    }
+}
